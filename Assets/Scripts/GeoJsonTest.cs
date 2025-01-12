5e4826d918d74ffbb988778c86689029137683eb
Matthias Heim <github@matthiasheim.eu>
2025-01-12 01:47:39 +0100

    Load GeoJson in AR Scene

--- Assets/Scripts/GeoJsonTest.cs
+++ Assets/Scripts/GeoJsonTest.cs
@@ -2,43 +2,55 @@ using GeoJSON.Net.Feature;
 using GeoJSON.Net.Geometry;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class GeoJsonTest : MonoBehaviour
 {
-    void Start()
+    public List<LineRenderer> lineStrings;
+    public List<Transform> PointsOfInterest;
+
+    [ContextMenu("Create GeoJson From LineRenderers And PointsOfInterest")]
+    public void CreateGeoJsonFromLineRenderersAndPointsOfInterest()
     {
-        Position position = new Position(51.899523, -2.124156);
-        Point point = new Point(position);
-
-        List<IPosition> linePositions = new List<IPosition>
+        List<Feature> features = new List<Feature>();
+        foreach (LineRenderer line in lineStrings)
         {
-            new Position(51.899523, -2.124156),
-            new Position(51.899523, -3.124156)
-        };
-        LineString lineString = new LineString(linePositions);
+            List<IPosition> linePositions = new List<IPosition>();
+            for (int i = 0; i < line.positionCount; i++)
+            {
+                Vector3 position = line.GetPosition(i);
+                linePositions.Add(new Position(position.x, position.z, position.y));
+            }
+            LineString lineString = new LineString(linePositions);
+            Feature lineStringFeature = new Feature(lineString);
+            lineStringFeature.Properties.Add("color", ColorToHex(line.startColor));
+            lineStringFeature.Properties.Add("width", line.startWidth);
+            features.Add(lineStringFeature);
+        }
 
-        Feature pointFeature = new Feature(point);
+        foreach (Transform point in PointsOfInterest)
+        {
+            Position position = new Position(point.position.x, point.position.z, point.position.y);
+            Point pointGeometry = new Point(position);
+            Feature pointFeature = new Feature(pointGeometry);
 
-        pointFeature.Properties.Add("color", ColorToHex(Color.red));
+            var poiColor = point.GetComponent<PoiColor>().color;
+            pointFeature.Properties.Add("color", ColorToHex(poiColor));
 
-        Feature lineStringFeature = new Feature(lineString);
-        lineStringFeature.Properties.Add("color", ColorToHex(Color.blue));
+            features.Add(pointFeature);
+        }
 
-        FeatureCollection featureCollection = new FeatureCollection(new List<Feature> { pointFeature, lineStringFeature });
+        FeatureCollection featureCollection = new FeatureCollection(features);
+        string json = JsonConvert.SerializeObject(featureCollection, Formatting.Indented);
 
-        string json = JsonConvert.SerializeObject(featureCollection);
-        Debug.Log(json);
+        string path = Path.Combine(Application.dataPath, "GeoJsonOutput.json");
+        File.WriteAllText(path, json);
+        Debug.Log($"GeoJSON saved to {path}");
     }
 
     string ColorToHex(Color color)
     {
         return $"#{ColorUtility.ToHtmlStringRGBA(color)}";
-    }
-
-    Color HexToColor(string hex)
-    {
-        ColorUtility.TryParseHtmlString(hex, out Color color);
-        return color;
     }
 }
---
+++ Assets/Scripts/GeoJsonVisualizer.cs
@@ -0,0 +1,76 @@
+using GeoJSON.Net.Feature;
+using GeoJSON.Net.Geometry;
+using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class GeoJsonVisualizer : MonoBehaviour
+{
+    public TextAsset geoJsonFile;
+    public GameObject pointPrefab;
+    public GameObject lineRendererPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string path = Path.Combine(Application.dataPath, "GeoJsonOutput.json");
+        if (File.Exists(path))
+        {
+            string json = geoJsonFile.text;
+            FeatureCollection featureCollection = JsonConvert.DeserializeObject<FeatureCollection>(json);
+
+            GameObject pointsParent = new GameObject("Points");
+            pointsParent.transform.SetParent(this.transform, false);
+
+            GameObject lineStringsParent = new GameObject("LineStrings");
+            lineStringsParent.transform.SetParent(this.transform, false);
+
+            foreach (Feature feature in featureCollection.Features)
+            {
+                if (feature.Geometry is Point point)
+                {
+                    Vector3 position = new Vector3((float)point.Coordinates.Latitude, (float)point.Coordinates.Altitude, (float)point.Coordinates.Longitude);
+                    GameObject pointInstance = Instantiate(pointPrefab, position, Quaternion.identity);
+                    pointInstance.transform.SetParent(pointsParent.transform, false);
+
+                    var color = HexToColor(feature.Properties.TryGetValue("color", out object colorString) ? (string)colorString : "#FFFFFF");
+                    pointInstance.GetComponent<Renderer>().material.color = color;
+                }
+                else if (feature.Geometry is LineString lineString)
+                {
+                    GameObject lineInstance = Instantiate(lineRendererPrefab);
+                    LineRenderer lineRenderer = lineInstance.GetComponent<LineRenderer>();
+                    lineRenderer.positionCount = lineString.Coordinates.Count;
+                    Vector3[] positions = new Vector3[lineString.Coordinates.Count];
+                    for (int i = 0; i < lineString.Coordinates.Count; i++)
+                    {
+                        positions[i] = new Vector3((float)lineString.Coordinates[i].Latitude, (float)lineString.Coordinates[i].Altitude, (float)lineString.Coordinates[i].Longitude);
+                    }
+                    lineRenderer.SetPositions(positions);
+
+                    var color = HexToColor(feature.Properties.TryGetValue("color", out object colorString) ? (string)colorString : "#FFFFFF");
+                    lineRenderer.startColor = color;
+                    lineRenderer.endColor = color;
+
+                    var width = feature.Properties.TryGetValue("width", out object widthValue) ? (float)(double)widthValue : 0.1f;
+                    lineRenderer.startWidth = width;
+                    lineRenderer.endWidth = width;
+
+                    lineInstance.transform.SetParent(lineStringsParent.transform, false);
+                }
+            }
+        }
+        else
+        {
+            Debug.LogError($"GeoJSON file not found at {path}");
+        }
+    }
+
+    Color HexToColor(string hex)
+    {
+        ColorUtility.TryParseHtmlString(hex, out Color color);
+        return color;
+    }
+}
